b13a0b213904480157d0a595fcb7052a1db435ab
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2022-07-25 14:13:43 +0200

    DoofahEndpoint: Super basic test implemenation

--- Doofah-Endpoint/src/main.cpp
+++ Doofah-Endpoint/src/main.cpp
@@ -1,59 +1,234 @@
 #include <Arduino.h>
 #include <BleKeyboard.h>
+#include <SimpleSerial.h>
+#include <EEPROM.h>
 
-BleKeyboard bleKeyboard("BLE Test Keyboard", "Metrological");
+#define EEPROM_SIZE 2
 
-enum Action : uint8_t {
-  KEY_SEND = 0x01,
-  KEY_PRESS,
-  KEY_RELEASE,
-  KEY_RELEASE_ALL,
-  BATTERY_LEVEL,
-  SET_VENDOR_ID,
-  SET_PRODUCT_ID,
-  SET_NAME,
-};
+using namespace WPEFramework::SimpleSerial;
 
-struct Frame
+static const char *ID()
 {
-  uint8_t length;
-  Action action;
-  uint8_t data[];
-};
+  static char id[23] = {};
 
-void setup() {
-  Serial.begin(115200);
-  bleKeyboard.begin();
+  if (strlen(id) == 0)
+  {
+    // The chip ID is essentially its MAC address(length: 6 bytes).
+    uint64_t chipid = ESP.getEfuseMac();
+    snprintf(id, 23, "Doofah-%04X%08X", (uint16_t)(chipid >> 32), (uint32_t)chipid);
+  }
+
+  return id;
 }
 
-void loop() {
-  if(bleKeyboard.isConnected()) {
-    Serial.println("Sending 'Hello world'...");
-    delay(10000);
-    bleKeyboard.print("Hello world");
+BleKeyboard bleKeyboard(ID(), "Metrological");
+Protocol::Message buffer;
+std::vector<Payload::Device> devices;
 
-    delay(1000);
 
-    Serial.println("Sending Enter key...");
-    bleKeyboard.write(KEY_RETURN);
+constexpr uint8_t InvalidEEPROMValue = 0xFF;
 
-    delay(1000);
+//EEPROM addresses
+constexpr uint8_t MessageBaseAddress = 0x00;
+constexpr uint8_t MessageOperationAddress = MessageBaseAddress;
+constexpr uint8_t MessageSequenceAddress = MessageBaseAddress + 1;
 
-    Serial.println("Sending Play/Pause media key...");
-    bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+void log(const char *fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
 
-    delay(1000);
+  std::vector<char> buffer(strlen(fmt));
 
-   //
-   // Below is an example of pressing multiple keyboard modifiers 
-   // which by default is commented out.
-    /*
-    Serial.println("Sending Ctrl+Alt+Delete...");
-    bleKeyboard.press(KEY_LEFT_CTRL);
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.press(KEY_DELETE);
-    delay(100);
-    bleKeyboard.releaseAll();
-    */
+  int sz = vsnprintf(&buffer[0], buffer.size(), fmt, args);
+
+  if (sz >= int(buffer.size()))
+  {
+    buffer.resize(sz + 1);
+    vsnprintf(&buffer[0], buffer.size(), fmt, args);
+  }
+  va_end(args);
+
+  Serial2.println(&buffer[0]);
+}
+
+Protocol::ResultType KeyMessage(const Payload::KeyEvent *event)
+{
+  uint8_t keycode(event->code & 0xff);
+
+  log("Sending key event: 0x%02X (0x%04X)", keycode, event->code);
+
+  if (event->pressed == Payload::Action::PRESSED)
+  {
+    bleKeyboard.press(keycode);
+  }
+  else
+  {
+    bleKeyboard.release(keycode);
+  }
+
+  return Protocol::ResultType::OK;
+}
+
+void SendMessage(const Protocol::Message &message)
+{
+  uint8_t data(0);
+  log("Sending %d bytes with operation=0x%02X result=0x%02X...", message.Size(), message.Operation(), message.Result());
+
+  while (message.Serialize(sizeof(data), &data) == 0)
+  {
+    Serial.write(data);
+  }
+}
+
+void Persist(const uint8_t operation, const uint8_t sequence)
+{
+  EEPROM.write(MessageOperationAddress, operation);
+  EEPROM.write(MessageSequenceAddress, sequence);
+  EEPROM.commit();
+}
+
+void HandleReboot()
+{
+  uint8_t operation = EEPROM.read(MessageOperationAddress);
+
+  if (operation != InvalidEEPROMValue)
+  {
+    log("Handle reset: 0x%02X was found on MessageOperationAddress", operation);
+
+    buffer.Operation(static_cast<Protocol::OperationType>(operation));
+    buffer.Sequence(EEPROM.read(MessageSequenceAddress));
+    buffer.Result(Protocol::ResultType::OK);
+    buffer.PayloadLength(0);
+
+    buffer.Finalize();
+
+    SendMessage(buffer);
+
+    buffer.Clear();
+    
+    // reset EEPROM
+    Persist(InvalidEEPROMValue, InvalidEEPROMValue);
+  }
+}
+
+void Process(Protocol::Message &message)
+{
+  Protocol::ResultType result(Protocol::ResultType::OK);
+
+  log("Processing %d 0x%02X...", message.Size(), message.Operation());
+
+  if (message.IsValid() == true)
+  {
+
+    Protocol::ResultType result = Protocol::ResultType::OPERATION_INVALID;
+    message.Offset(0);
+
+    switch (message.Operation())
+    {
+    case Protocol::OperationType::ALLOCATE:
+      result = Protocol::ResultType::OK;
+      message.PayloadLength(0);
+      break;
+
+    case Protocol::OperationType::EVENT:
+      result = Protocol::ResultType::OK;
+      message.PayloadLength(0);
+      break;
+
+    case Protocol::OperationType::FREE:
+      result = Protocol::ResultType::OK;
+      message.PayloadLength(0);
+
+      break;
+
+    case Protocol::OperationType::KEY:
+      if (message.PayloadLength() == sizeof(Payload::KeyEvent))
+      {
+        result = KeyMessage(reinterpret_cast<const Payload::KeyEvent *>(message.Payload()));
+      }
+
+      message.PayloadLength(0);
+      break;
+
+    case Protocol::OperationType::RESET:
+      if (message.Address() == static_cast<Protocol::DeviceAddressType>(Payload::Peripheral::ROOT))
+      {
+        Persist(static_cast<uint8_t>(message.Operation()), message.Sequence());
+        ESP.restart();
+        result = Protocol::ResultType::OK;
+        message.PayloadLength(0);
+      }
+      break;
+
+    case Protocol::OperationType::SETTINGS:
+      result = Protocol::ResultType::OK;
+      message.PayloadLength(0);
+      break;
+
+    case Protocol::OperationType::STATE:
+      message.Offset(0);
+      message.PayloadLength(devices.size() * sizeof(Payload::Device));
+
+      for (const auto &dev : devices)
+      {
+        message.Deserialize(sizeof(dev), reinterpret_cast<const uint8_t *>(&dev));
+      }
+
+      result = Protocol::ResultType::OK;
+      break;
+
+    default:
+      message.PayloadLength(0);
+      result = Protocol::ResultType::OPERATION_INVALID;
+      break;
+    }
+
+    message.Result(result);
+  }
+  else
+  {
+    message.PayloadLength(0);
+    message.Result(Protocol::ResultType::CRC_INVALID);
+  }
+
+  message.Finalize();
+  message.Offset(0);
+
+  SendMessage(message);
+
+  message.Clear();
+}
+
+void setup()
+{
+  EEPROM.begin(EEPROM_SIZE);
+  Serial2.begin(LOGBAUDRATE, SERIAL_8N1, LOGRXPIN, LOGTXPIN);
+
+  log("Starting %s endpoint build %s", ID(), __TIMESTAMP__);
+  
+  buffer.Clear();
+
+  devices.push_back({0x00, Payload::PeripheralState::AVAILABLE, Payload::Peripheral::ROOT});
+  devices.push_back({0x01, Payload::PeripheralState::AVAILABLE, Payload::Peripheral::BLE});
+
+  bleKeyboard.begin();
+  Serial.begin(115200);
+
+  HandleReboot();
+}
+
+void loop()
+{
+  while (Serial.available() > 0)
+  {
+    uint8_t byte = Serial.read();
+    buffer.Deserialize(sizeof(byte), &byte);
+
+    if (buffer.IsComplete())
+    {
+      log("Received a complete message!");
+      Process(buffer);
+    }
   }
 }
