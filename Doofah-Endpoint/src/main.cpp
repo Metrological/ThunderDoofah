631cb294a3e82efc3f9a6abc2a08c2a66907d2b4
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2022-08-10 11:56:45 +0200

    Doofah-Endpoint: Refactor main program

--- Doofah-Endpoint/src/main.cpp
+++ Doofah-Endpoint/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
-#include "Log.h"
 
-#include <BleKeyboard.h>
-#include <EEPROM.h>
+#include <Controller.h>
+#include <Log.h>
+
+#include <BleKeyboardDevice.h>
+#include <IRKeyboardDevice.h>
+
 #include <NeoPixelBus.h>
-#include <NimBLEDevice.h>
 #include <OneButton.h>
 #include <SimpleSerial.h>
 
-using namespace WPEFramework::SimpleSerial;
+#include <string>
 
-BleKeyboard bleKeyboard("Doofah", "Metrological");
+using namespace WPEFramework::SimpleSerial;
+using namespace Doofhah;
+
+static Controller::Peripheral<BleKeyboardDevice> ble("Doofah", "Metrological");
+static Controller::Peripheral<IRKeyboardDevice> ir(IR_TX_PIN);
+
 Protocol::Message buffer;
-std::vector<Payload::Device> devices;
 
 OneButton button = OneButton(
     BUTTON_PIN, // Input pin for the button
@@ -43,106 +49,22 @@ void Led(const RgbColor& color)
     led.Show();
 }
 
-constexpr uint8_t IRDevices[] = {
-    IR_TX_PIN
-};
-
-// EEPROM addresses
-constexpr uint8_t MessageBaseAddress = 0x00;
-
-#pragma pack(push, 1)
-struct Config {
-    // The default values if there is no EEPROM setting yet..
-    Config()
-        : ble()
-        , ir()
-        , battery()
-    {
-        memset(&ble, 0, sizeof(ble));
-        memset(&ir, 0, sizeof(ir));
-        memset(&battery, 0, sizeof(battery));
-    }
-    Payload::BLESettings ble;
-    Payload::IRSettings ir;
-    Payload::BatteryLevel battery;
-} _config;
-#pragma pack(pop)
-
-void Save()
-{
-    EEPROM.writeBytes(MessageBaseAddress, &_config, sizeof(_config));
-    EEPROM.commit();
-}
-void Load()
-{
-    if (EEPROM.readByte(MessageBaseAddress) != 0xFF) {
-        EEPROM.readBytes(MessageBaseAddress, &_config, sizeof(_config));
-    }
-}
-
-bool SetBLE(const Payload::BLESettings& newSettings)
-{
-    bool changed;
-
-    if ((memcmp(&newSettings, &_config.ble, std::min(sizeof(newSettings), sizeof(_config.ble))) != 0)) {
-        memcpy(&_config.ble, &newSettings, std::min(sizeof(newSettings), sizeof(_config.ble)));
-        changed = true;
-    }
-
-    return changed;
-}
-bool SetIR(const Payload::IRSettings& newSettings)
-{
-    bool changed;
-
-    if ((memcmp(&newSettings, &_config.ir, std::min(sizeof(newSettings), sizeof(_config.ir))) != 0)) {
-        memcpy(&_config.ir, &newSettings, std::min(sizeof(newSettings), sizeof(_config.ir)));
-        changed = true;
-    }
-
-    return changed;
-}
-bool SetBattery(const Payload::BatteryLevel& newSettings)
-{
-    bool changed;
-
-    if ((memcmp(&newSettings, &_config.battery, std::min(sizeof(newSettings), sizeof(_config.battery))) != 0)) {
-        memcpy(&_config.battery, &newSettings, std::min(sizeof(newSettings), sizeof(_config.battery)));
-
-        (newSettings.percentage == 0) ? bleKeyboard.setBatteryLevel(100) : bleKeyboard.setBatteryLevel(newSettings.percentage);
-        changed = true;
-    }
-
-    return changed;
-}
-
 void FactoryReset()
 {
     Serial.flush();
     Serial.end();
 
-    Config cleared;
-
-    if (SetBLE(cleared.ble) || SetIR(cleared.ir) || SetBattery(cleared.battery)) {
-        Save();
-    }
+    Storage::Instance().Reset();
 
     ESP.restart();
 }
 
-Protocol::ResultType KeyMessage(const Payload::KeyEvent& event)
+void PrintMessage(const std::string& prefix, const Protocol::Message& message)
 {
-    uint8_t keycode(event.code & 0xff);
+    std::string data;
+    ToHexString(message.Data(), message.Size(), data);
 
-    Log::Instance().Print("Sending key event: 0x%02X (0x%04X)", keycode, event.code);
-
-    if (event.pressed == Payload::Action::PRESSED) {
-        bleKeyboard.press(keycode);
-    } else {
-        bleKeyboard.release(keycode);
-    }
-
-    return Protocol::ResultType::OK;
+    GLOBAL_TRACE("%s: message[%s]", prefix.c_str(), data.c_str());
 }
 
 void SendMessage(const Protocol::Message& message)
@@ -152,7 +74,9 @@ void SendMessage(const Protocol::Message& message)
 
     uint8_t data(0);
 
-    Log::Instance().Print("Sending %d bytes with operation=0x%02X result=0x%02X...", message.Size(), message.Operation(), message.Result());
+    GLOBAL_TRACE("Sending %d bytes with operation=0x%02X result=0x%02X...", message.Size(), message.Operation(), message.Result());
+
+    PrintMessage(__FUNCTION__, message);
 
     while (message.Serialize(sizeof(data), &data) != 0) {
         Serial.write(data);
@@ -164,7 +88,9 @@ void Process(Protocol::Message& message)
 {
     bool reboot = false;
 
-    Log::Instance().Print("Processing %d bytes with operation=0x%02X device=0x%02X...", message.Size(), message.Operation(), message.Address());
+    GLOBAL_TRACE("Processing %d bytes with operation=0x%02X device=0x%02X...", message.Size(), message.Operation(), message.Address());
+
+    PrintMessage(__FUNCTION__, message);
 
     if (message.IsValid() == false) {
         message.PayloadLength(0);
@@ -174,98 +100,58 @@ void Process(Protocol::Message& message)
 
         switch (message.Operation()) {
         case Protocol::OperationType::KEY:
-            if (message.PayloadLength() == sizeof(Payload::KeyEvent)) {
-                result = KeyMessage(*(reinterpret_cast<const Payload::KeyEvent*>(message.Payload())));
+
+            GLOBAL_TRACE("KeyEvent of 0x%02X", message.Address());
+            if ((message.Address() > 0x00) && (message.PayloadLength() == sizeof(Payload::KeyEvent))) {
+                result = Controller::Instance().KeyEvent(message.Address() - 1, *(reinterpret_cast<const Payload::KeyEvent*>(message.Payload())));
             }
             message.PayloadLength(0);
             break;
 
         case Protocol::OperationType::RESET:
+            GLOBAL_TRACE("Reset settings of 0x%02X", message.Address());
             if (message.Address() == 0x00) {
                 reboot = true;
                 result = Protocol::ResultType::OK;
-            } else if (message.Address() == 0x01) {
-                Payload::BLESettings clean;
-                memset(&clean, 0, sizeof(clean));
+            } else {
+                result = Controller::Instance().Reset(message.Address() - 1);
+            }
 
-                if (SetBLE(clean) == true) {
-                    Save();
-                    reboot = true;
-                }
-                result = Protocol::ResultType::OK;
-            }
-#ifdef IR_ENABLED
-            else if (message.Address() == 0x02) {
-                Payload::IRSettings clean;
-                memset(&clean, 0, sizeof(clean));
-
-                if (SetIR(clean) == true) {
-                    Save();
-                    reboot = true;
-                }
-                result = Protocol::ResultType::OK;
-            }
-#endif
-            else {
-                result = Protocol::ResultType::NOT_AVAILABLE;
-            }
             message.PayloadLength(0);
             break;
 
         case Protocol::OperationType::SETTINGS:
-            if (message.Address() == 0x01) {
-                if (message.PayloadLength() == sizeof(Payload::BLESettings)) {
-                    Log::Instance().Print("BLE Settings");
-
-                    Payload::BLESettings newSettings;
-
-                    memcpy(&newSettings, message.Payload(), message.PayloadLength());
-
-                    if (SetBLE(newSettings) == true) {
-                        Save();
-                    }
-
-                    result = Protocol::ResultType::OK;
-                } else {
-                    result = Protocol::ResultType::PAYLOAD_INVALID;
-                }
+            GLOBAL_TRACE("Set settings of 0x%02X", message.Address());
+            if (message.Address() > 0x00) {
+                result = Controller::Instance().Setup(message.Address() - 1, message.PayloadLength(), message.Payload());
             }
-#ifdef IR_ENABLED
-            else if (message.Address() == 0x02) {
-
-                if (message.PayloadLength() == sizeof(Payload::IRSettings)) {
-                    Payload::IRSettings newSettings;
-
-                    memcpy(&newSettings, message.Payload(), message.PayloadLength());
-
-                    if (SetIR(newSettings) == true) {
-                        Save();
-                    }
-
-                    result = Protocol::ResultType::OK;
-                } else {
-                    result = Protocol::ResultType::PAYLOAD_INVALID;
-                }
-            }
-#endif
-            else {
-                result = Protocol::ResultType::NOT_AVAILABLE;
-            }
-
             message.PayloadLength(0);
-
             break;
 
         case Protocol::OperationType::STATE: {
             uint8_t offset(0);
-            uint8_t payload[devices.size() * sizeof(Payload::Device)];
+            GLOBAL_TRACE("Report devices");
+
+            const Controller::DeviceList& devices = Controller::Instance().Devices();
+
+            std::vector<Payload::Device> payload;
+
+            payload.push_back({ 0x00, Payload::PeripheralState::AVAILABLE, Payload::Peripheral::ROOT });
 
             for (const auto& dev : devices) {
-                memcpy(&payload[offset], reinterpret_cast<const uint8_t*>(&dev), sizeof(Payload::Device));
+                Payload::Device device;
+                device.address = (offset / sizeof(Payload::Device)) + 1;
+                device.state = Payload::PeripheralState::AVAILABLE;
+                device.peripheral = dev->Type();
+                payload.push_back(device);
+
+                GLOBAL_TRACE("Adding device address=0x%02X type=0x%02X", device.address, device.peripheral);
+
                 offset += sizeof(Payload::Device);
             }
 
-            message.Payload(sizeof(payload), payload);
+            message.Payload((payload.size() * sizeof(Payload::Device)), reinterpret_cast<uint8_t*>(payload.data()));
+
             result = Protocol::ResultType::OK;
             break;
         }
@@ -340,20 +226,9 @@ void setup()
     led.Begin();
     Led(red);
 
-    EEPROM.begin(sizeof(_config));
-    Load();
+    Storage::Instance().Begin();
 
     buffer.Clear();
-
-    devices.push_back({ 0x00, Payload::PeripheralState::AVAILABLE, Payload::Peripheral::ROOT });
-    devices.push_back({ 0x01, Payload::PeripheralState::AVAILABLE, Payload::Peripheral::BLE });
-
-#ifdef IR_ENABLED
-    uint8_t deviceAdressIndex(0);
-    while (deviceAdressIndex < sizeof(IRDevices)) {
-        devices.push_back({ IRDevices[deviceAdressIndex++], Payload::PeripheralState::AVAILABLE, Payload::Peripheral::IR });
-    }
-#endif
 
     button.setPressTicks(longPressTimeMs);
     button.attachClick(SingleClick);
@@ -361,32 +236,12 @@ void setup()
     button.attachLongPressStop(PressStop);
     button.attachDuringLongPress(PressUpdate);
 
-    if (strlen(_config.ble.name) > 0) {
-        bleKeyboard.setName(_config.ble.name);
-        Log::Instance().Print("BLE name %s loaded from flash", _config.ble.name);
-    }
-
-    if (_config.ble.vid > 0) {
-        bleKeyboard.set_vendor_id(_config.ble.vid);
-        Log::Instance().Print("BLE vendor ID 0x%04X loaded from flash", _config.ble.vid);
-    }
-
-    if (_config.ble.pid > 0) {
-        bleKeyboard.set_product_id(_config.ble.pid);
-        Log::Instance().Print("BLE product ID 0x%04X loaded from flash", _config.ble.pid);
-    }
-
-    if (_config.battery.percentage > 0) {
-        bleKeyboard.setBatteryLevel(_config.battery.percentage);
-        Log::Instance().Print("BLE batery level %d% loaded from flash", _config.battery.percentage);
-    }
-
-    bleKeyboard.begin();
+    Controller::Instance().StartDevices();
 
     Serial.begin(COM_BAUDRATE);
 
-    NimBLEAddress address = NimBLEDevice::getAddress();
-    Log::Instance().Print("Starting BLE work on [%s] endpoint build %s", address.toString().c_str(), __TIMESTAMP__);
+    GLOBAL_TRACE("Starting endpoint build %s", __TIMESTAMP__);
+
     SendEvent();
     Led(off);
 }
@@ -401,7 +256,7 @@ void loop()
         buffer.Deserialize(sizeof(byte), &byte);
 
         if (buffer.IsComplete()) {
-            Log::Instance().Print("Received a complete message!");
+            GLOBAL_TRACE("Received a complete message!");
             Process(buffer);
             Led(off);
         }
